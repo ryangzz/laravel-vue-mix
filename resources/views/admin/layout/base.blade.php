eff1c0446060ca36a4da7805d54eaff27bb3c02f
Gonzalo Cavazos <gonzalo.cavazos@metodika.mx>
2022-08-28 15:34:24 -0500

    font poppins

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -6,10 +6,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
   <title>{{ config('app.name' )}} | Starter</title>
       
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap" rel="stylesheet">
   @include('admin.layout.styles')
+  <style>
+    html *:not(i):not(span){
+      font-family: 'Poppins' !important
+    }
+  </style>
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
